4577d6b2604f1be7d7681ba84eabb86c1edb6a57
Protick Roy <protickrr@gmail.com>
2023-10-12 23:22:43 +0600

    added styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,47 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
+
+const StyledApp = styled.div`
+  /* background-color: orangered; */
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button onClick={(e) => alert("check in")}>Check in</Button>
+              <Button
+                variation="secondary"
+                size="small"
+                onClick={(e) => alert("check out")}
+              >
+                Check out
+              </Button>
+            </div>
+          </Row>
+
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number Of Guests" />
+              <Input type="number" placeholder="Number Of Guests" />
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
